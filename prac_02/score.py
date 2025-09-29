0ea03c5a5a5c8175d6f91c27a0fa8b6fbf07e487
Dawe-D <dylanhdawe@gmail.com>
2025-09-29 14:09:02 +1000

    Revert "Add and Update prac 2 files"
    This reverts commit baafbfb7d152997c146ce98adbdbea02b74ed545.

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,33 +2,13 @@
 CP1404/CP5632 - Practical
 Program to determine score status
 """
-import random
 
-
-def main():
-    score = get_score()
-    result = determine_result(score)
-    print(result)
-
-    random_score = random.randint(0, 100)
-    random_result = determine_result(random_score)
-    print(random_score)
-    print(random_result)
-
-def determine_result(score: float):
-    if score < 0 or score > 100:
-        return "Invalid score"
-    elif score >= 90:
-        return "Excellent"
-    elif score >= 50:
-        return "Passable"
-    else:
-        return "Bad"
-
-
-def get_score() -> float:
-    score = float(input("Enter score: "))
-    return score
-
-
-main()
+score = float(input("Enter score: "))
+if score < 0 or score > 100:
+    print("Invalid score")
+elif score >= 90:
+    print("Excellent")
+elif score >= 50:
+    print("Passable")
+else:
+    print("Bad")
