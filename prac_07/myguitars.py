c0a22c7543c8faa21ca45d7a90f828292b46daa2
Dawe-D <dylanhdawe@gmail.com>
2025-11-09 13:32:33 +1000

    Complete More Guitars tasks

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -7,15 +7,20 @@ from guitar import Guitar
 
 def main():
     guitars = []
+    load_guitars(guitars)
+    print_guitars(guitars)
+
+def print_guitars(guitars):
+    for guitar in sorted(guitars):
+        print(guitar)
+
+
+def load_guitars(guitars):
     in_file = open("guitars.csv", "r")
     for guitar in in_file:
         parts = guitar.strip().split(',')
         guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
         guitars.append(guitar)
     in_file.close()
-
-    guitars.sort()
-    for guitar in guitars:
-        print(guitar)
 
 main()
